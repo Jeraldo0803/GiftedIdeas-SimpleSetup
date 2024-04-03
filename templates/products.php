ddc47e2e687359e1bc41477c2e0d8f46425c2e42
neonnick21 <neonnick21@gmail.com>
2024-04-03 23:18:48 +0800

    Update products.php
    sorting functionality hotfix 4 aaaaa

--- templates/products.php
+++ templates/products.php
@@ -12,14 +12,14 @@
     <link rel="stylesheet" href="../assets/css/Hero-Clean-Reverse-images.css">
     <link rel="stylesheet" href="../assets/css/Lightbox-Gallery-baguetteBox.min.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
-	
+
     <!-- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
     <div data-include="templates/header"></div>
-	
+    
     <section class="photo-gallery py-4 py-xl-5">
         <div class="container">
             <div class="row mb-5">
@@ -32,90 +32,57 @@
                     </div>
                 </div>
             </div>
-<<<<<<< Updated upstream
-            <div class="row gx-5 gy-5 row-cols-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-4 photos" id="main" data-bss-baguettebox="">
-                <div class="col item" id="dv_7"><a href="../assets/img/product1shirt.jpg">
-=======
             <div class="row gx-5 gy-5 row-cols-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-4 photos" id="main">
                 <div class="col item" id="dv_7"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product1shirt.jpg">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Shirt</h4>
                             </div>
                         </div>
                     </a></div>
-<<<<<<< Updated upstream
-                <div class="col item" id="dv_3"><a href="../assets/img/product2mug.jpg">
-=======
                 <div class="col item" id="dv_3"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product2mug.jpg">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Mug</h4>
                             </div>
                         </div>
                     </a></div>
-<<<<<<< Updated upstream
-                <div class="col item" id="dv_1"><a href="../assets/img/product3cup.jpg">
-=======
                 <div class="col item" id="dv_1"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product3cup.jpg">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Cup</h4>
                             </div>
                         </div>
                     </a></div>
-<<<<<<< Updated upstream
-                <div class="col item" id="dv_4"><a href="../assets/img/product4pen.jpg">
-=======
                 <div class="col item" id="dv_4"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product4pen.jpg" style="background: #ffffff;">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Pen</h4>
                             </div>
                         </div>
                     </a></div>
-<<<<<<< Updated upstream
-                <div class="col item" id="dv_2"><a href="../assets/img/product5jacket.jpg">
-=======
                 <div class="col item" id="dv_2"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product5jacket.jpg" style="background: #ffffff;">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Jacket</h4>
                             </div>
                         </div>
                     </a></div>
-<<<<<<< Updated upstream
-                <div class="col item" id="dv_6"><a href="../assets/img/product6pillow.jpg">
-=======
                 <div class="col item" id="dv_6"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product6pillow.jpg" style="background: #ffffff;" width="254" height="135">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Pillow case</h4>
                             </div>
                         </div>
                     </a></div>
-<<<<<<< Updated upstream
-                <div class="col item" id="dv_5"><a href="../assets/img/product7phonecase.jpg">
-=======
                 <div class="col item" id="dv_5"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product7phonecase.jpg" style="background: #ffffff;">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Phone case</h4>
                             </div>
                         </div>
                     </a></div>
-<<<<<<< Updated upstream
-                <div class="col item" id="dv_8"><a href="../assets/img/product8totebag.jpg">
-=======
                 <div class="col item" id="dv_8"><a href="product_editor.php">
->>>>>>> Stashed changes
                         <div class="card">
                             <div class="card-body" style="background: #dedede;text-align: center;"><img class="img-fluid" src="../assets/img/product8totebag.jpg" style="background: #ffffff;">
                                 <h4 class="card-title" style="font-family: 'Montserrat Alternates', sans-serif;font-weight: bold;">Tote bag</h4>
@@ -125,34 +92,9 @@
             </div>
         </div>
     </section>
-<<<<<<< Updated upstream
-	
-	<script>
-function sortasc(){    
-var main = document.getElementById( 'main' );
-
-[].map.call( main.children, Object ).sort( function ( a, b ) {
-    return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
-}).forEach( function ( elem ) {
-    main.appendChild( elem );
-});
-}
-    
-function sortdesc(){    
-var main = document.getElementById( 'main' );
-
-[].map.call( main.children, Object ).sort( function ( b, a ) {
-    return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
-}).forEach( function ( elem ) {
-    main.appendChild( elem );
-});
-}
-</script>
-=======
 	<script>
 	function sortasc(){    
 	var main = document.getElementById( 'main' );
->>>>>>> Stashed changes
 
 	[].map.call( main.children, Object ).sort( function ( a, b ) {
 		return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
